a2262d1e24e6b38ad4d2bea5d6965231c2832a7c
EmadAkroush <emadakroush@gmail.com>
2021-09-25 17:44:15 +0330

    tow

--- functions.php
+++ functions.php
@@ -4,8 +4,8 @@
 
 function childtheme_enqueue_style() {
     wp_enqueue_style( 'parent-style', get_template_directory_uri().'/style.css');
-    wp_register_script( 'child-style' , get_stylesheet_directory_uri().'/style.css' );	
-wp_enqueue_script( 'custom' , get_stylesheet_directory_uri().'/js/custom.js' );
+    wp_enqueue_style( 'child-style' , get_stylesheet_directory_uri().'/style.css' );	
+    wp_enqueue_script( 'custom' , get_stylesheet_directory_uri().'/js/custom.js' );
 }
 add_action ('wp_enqueue_scripts' , 'childtheme_enqueue_style' , 999 );
 

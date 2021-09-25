374c5bac8f076c75f35bd60b1af94ba575713896
EmadAkroush <emadakroush@gmail.com>
2021-09-26 00:21:27 +0330

    first commit

--- footer.php
+++ footer.php
@@ -1,1 +1,44 @@
-<script type="text/javascript" src="<?php bloginfo('template_url')?>/js/custom.js"> </script>
+
+
+
+<?php
+/**
+ * The template for displaying the footer.
+ *
+ * Contains the closing of the #content div and all content after.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package Astra
+ * @since 1.0.0
+ */
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit; // Exit if accessed directly.
+}
+
+?>
+<?php astra_content_bottom(); ?>
+	</div> <!-- ast-container -->
+	</div><!-- #content -->
+<?php 
+	astra_content_after();
+		
+	astra_footer_before();
+		
+	astra_footer();
+		
+	astra_footer_after(); 
+?>
+	</div><!-- #page -->
+<?php 
+	astra_body_bottom();    
+	wp_footer(); 
+?>
+<!-- Google Tag Manager (noscript) -->
+<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-KB856XD"
+height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+<!-- End Google Tag Manager (noscript) -->
+<script type="text/javascript" src="<?php echo get_stylesheet_directory_uri()?>/js/custom.js"> </script>
+	</body>
+</html>
---
+++ js/custom.js
@@ -0,0 +1,89 @@
+//document.getElementsByClassName("show")[0].addEventListener("click",function(){
+ 
+    
+  // var x.document.getElementsByClassName("toggel");
+   // for(var i=0; i<=x.length; i++){
+ 
+  //  document.getElementsByClassName("toggel")[i].style.display="block";
+    
+  //  }
+
+    
+//}); 
+
+
+function my(){
+    var x=document.getElementsByClassName("toggel");
+  for(var i=0; i<=x.length; i++){
+
+  document.getElementsByClassName("toggel")[i].style.display="block";
+  document.getElementsByClassName("showa")[0].style.display="block";
+      document.getElementsByClassName("logo-body")[0].style.height="2250px";
+  };
+  
+ 
+};
+function mya(){
+    var x=document.getElementsByClassName("toggel");
+  for(var i=0; i<=x.length; i++){
+
+  document.getElementsByClassName("toggel")[i].style.display="none";
+  document.getElementsByClassName("showa")[0].style.display="block";
+      document.getElementsByClassName("logo-body")[0].style.height="1620px";
+
+  };
+
+};
+
+
+//document.getElementsByClassName("showa")[0].addEventListener("click",function(){
+ // document.getElementsByClassName("section-eight")[0].style.display="none";
+ // document.getElementsByClassName("section-nine")[0].style.display="none";
+ // document.getElementsByClassName("section-ten")[0].style.display="none";
+ // document.getElementsByClassName("section-eleven")[0].style.display="none";
+ // document.getElementsByClassName("showa")[0].style.display="none";
+  
+  
+  // for (var i=0;i<=65;i++){
+  
+  //document.getElementsByClassName("hover")[i].style.height= "1.9%";
+//  document.getElementsByClassName("hover")[i].style.marginTop= "145px";
+//  document.getElementsByClassName("colb")[i].addEventListener("mouseover",function(){
+  //    document.getElementsByClassName("colb")[i].style.width="calc(13% + 2rem)";
+   //   document.getElementsByClassName("colb")[i].style.height="calc(9% + 2.5rem)";
+  //    });
+//   };
+//});
+
+
+// first button min-width: 700px
+//function myFunctionOpen(x){
+   //if(x.matches){
+    
+    //document.getElementsByClassName("Logo")[0].style.height="2400px";
+    //document.getElementsByClassName("show")[0].style.marginRight = "52%";
+    
+      // for(var i=0;i<=65;i++){
+     // document.getElementsByClassName("colb")[i].style.height= "7%";
+   //    }
+//   }else  {
+     // document.getElementsByClassName("Logo")[0].style.height="3350px";
+     // for (var i=0;i<=65;i++){
+     //     document.getElementsByClassName("colb")[i].style.height= "3.5%";
+       //   document.getElementsByClassName("show")[0].style.marginRight = "52%";
+         // document.getElementsByClassName("showa")[0].style.marginRight = "13%";
+     // };
+  //}
+
+
+
+  //for (var i=0;i<=65;i++){
+  
+  //document.getElementsByClassName("hover")[i].style.height= "1.3%";
+  //document.getElementsByClassName("hover")[i].style.marginTop= "155px";
+  //document.getElementsByClassName("colb")[i].addEventListener("mouseover",function(){
+    //  document.getElementsByClassName("colb")[i].style.width="calc(13% + 2rem)";
+  //    document.getElementsByClassName("colb")[i].style.height="calc(9% + 2.5rem)";
+    //  });
+  //    };
+

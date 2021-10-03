21d906e39dc9c6ae2633c5fe3544635ca1029921
EmadAkroush <emadakroush@gmail.com>
2021-10-03 20:22:20 +0330

    new commit

--- js/custom.js
+++ js/custom.js
@@ -36,6 +36,31 @@ function mya(){
 };
 
 
+const accordionItemHeaders = document.querySelectorAll(".accordion-item-header");
+
+accordionItemHeaders.forEach(accordionItemHeader => {
+  accordionItemHeader.addEventListener("click", event => {
+    
+    // Uncomment in case you only want to allow for the display of only one collapsed item at a time!
+    
+    // const currentlyActiveAccordionItemHeader = document.querySelector(".accordion-item-header.active");
+    // if(currentlyActiveAccordionItemHeader && currentlyActiveAccordionItemHeader!==accordionItemHeader) {
+    //   currentlyActiveAccordionItemHeader.classList.toggle("active");
+    //   currentlyActiveAccordionItemHeader.nextElementSibling.style.maxHeight = 0;
+    // }
+
+    accordionItemHeader.classList.toggle("active");
+    const accordionItemBody = accordionItemHeader.nextElementSibling;
+    if(accordionItemHeader.classList.contains("active")) {
+      accordionItemBody.style.maxHeight = accordionItemBody.scrollHeight + "px";
+    }
+    else {
+      accordionItemBody.style.maxHeight = 0;
+    }
+    
+  });
+});
+
 //document.getElementsByClassName("showa")[0].addEventListener("click",function(){
  // document.getElementsByClassName("section-eight")[0].style.display="none";
  // document.getElementsByClassName("section-nine")[0].style.display="none";
